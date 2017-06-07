a55f27ac3a8f308adf68558e92b7987272759f71
shale <shale99@users.noreply.github.com>
2017-06-07 11:08:46 +0300

    additional code

--- src/main/java/org/folio/rest/impl/UsersAPI.java
+++ src/main/java/org/folio/rest/impl/UsersAPI.java
@@ -11,6 +11,9 @@ import org.folio.rest.tools.client.HttpModuleClient;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * @author shale
@@ -19,9 +22,10 @@ import io.vertx.core.Handler;
 public class UsersAPI implements UsersResource {
 
   private static String OKAPI_URL_HEADER = "X-Okapi-URL";
-  private static String OKAPI_TOKEN_HEADER = "X-Okapi-Token";
   private static String OKAPI_TENANT_HEADER = "X-Okapi-Tenant";
-  private static String OKAPI_PERMISSIONS_HEADER = "X-Okapi-Permissions";
+  private static String USERS_ENTRY = "users";
+
+  private final Logger logger = LoggerFactory.getLogger(UsersAPI.class);
 
   private boolean isBetween(int x, int min, int max) {
       return x>=min && x<=max;
@@ -48,15 +52,42 @@ public class UsersAPI implements UsersResource {
     org.folio.rest.tools.client.Response response = client.request("/users/" + userid, okapiHeaders);
     int statusCode = response.getCode();
     if(isBetween(statusCode, 200, 300)){
+      //can use BuildCQL object to get value in json as well
+      String userName = response.getBody().getString("username");
+      response = client.request("/users?query=username=" + userName, okapiHeaders);
+      statusCode = response.getCode();
+      if(isBetween(statusCode, 200, 300)){
+        JsonObject result = response.getBody();
+        Integer totalRecords = result.getInteger("total_records");
+        if(totalRecords == null || totalRecords < 1) {
+          asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+            GetUsersByIdByUseridResponse.withPlainNotFound("Unable to find user " + userName)));
+        } else if(totalRecords > 1) {
+          asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+            GetUsersByIdByUseridResponse.withPlainBadRequest("'" +
+                userName + "' is not unique")));
+        } else {
+          JsonObject record = result.getJsonArray(USERS_ENTRY).getJsonObject(0);
+          logger.debug("Got record " + record.encode());
+          response = client.request("/authn/credentials/" + userName, okapiHeaders);
 
+        }
+      }
     }
-    else if(statusCode == 404){
-      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
-        GetUsersByIdByUseridResponse.withPlainNotFound(response.getError().encodePrettily())));
-    }
-    else{
-      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
-        GetUsersByIdByUseridResponse.withPlainInternalServerError(response.getError().encodePrettily())));
+
+    if(!isBetween(statusCode, 200, 300)){
+      if(statusCode == 404){
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+          GetUsersByIdByUseridResponse.withPlainNotFound(response.getError().encodePrettily())));
+      }
+      else if(statusCode == 400){
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+          GetUsersByIdByUseridResponse.withPlainBadRequest(response.getError().encodePrettily())));
+      }
+      else{
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+          GetUsersByIdByUseridResponse.withPlainInternalServerError(response.getError().encodePrettily())));
+      }
     }
 
   }
